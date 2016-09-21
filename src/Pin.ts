238bec96ee3ba3a4070059cc9e06b8cae62e47c2
NoHomey <ivo.stratev.tues@gmail.com>
2016-09-21 15:57:40 +0300

    passing Pin->constructor->thorws Error describing that pin is in use and to call .release() if pin change is wanted when pin is in use

--- src/Pin.ts
+++ src/Pin.ts
@@ -3,7 +3,7 @@ export class Pin {
 	
     public constructor(private pin: number) {
 		if(Pin.inUse.indexOf(pin) !== -1) {
-			throw new Error();	
+			throw new Error(`pin: ${pin} is in use, call .release() on the first instance constructed with pin: ${pin} to change pin mode or settings`);	
 		} else {
 			Pin.inUse.push(pin);
 		}
