f866b0a8f21856ac5a90fc1cd89f2c180e3d9429
NoHomey <ivo.stratev.tues@gmail.com>
2016-09-21 15:59:16 +0300

    testing that release marks pin as not in use

--- __tests__/Pin.spec.ts
+++ __tests__/Pin.spec.ts
@@ -13,11 +13,21 @@ describe('Pin', () => {
             }).toThrowError(Error);
         });
 
-        it('thorws Error describing that pin is in use and to call .release() if pin change is wanted when pin is in use', () => {
+       it('thorws Error describing that pin is in use and to call .release() if pin change is wanted when pin is in use', () => {
             expect(() => {
                 let notInUse: Pin = new Pin(6);
                 let inUse: Pin = new Pin(6);
             }).toThrowError('pin: 6 is in use, call .release() on the first instance constructed with pin: 6 to change pin mode or settings');
+        });
+    });
+
+    describe('release', () => {
+        it('releases the pin which prevents errors to be throw when constructing new instance', () => {
+            expect(() => {
+                let notInUse: Pin = new Pin(3);
+                notInUse.release();
+                let inUse: Pin = new Pin(3);
+            }).not.toThrowError();
         });
     });
 });
