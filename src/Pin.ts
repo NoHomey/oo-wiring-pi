229ca346752a85e938f606476a9c7e5ef95730ff
NoHomey <ivo.stratev.tues@gmail.com>
2016-09-21 16:00:24 +0300

    passing last test

--- src/Pin.ts
+++ src/Pin.ts
@@ -1,13 +1,17 @@
 export class Pin {
-    private static inUse: Array<number> = [];
+	private static inUse: Array<number> = [];
 	
-    public constructor(private pin: number) {
+	public constructor(private pin: number) {
 		if(Pin.inUse.indexOf(pin) !== -1) {
 			throw new Error(`pin: ${pin} is in use, call .release() on the first instance constructed with pin: ${pin} to change pin mode or settings`);	
 		} else {
 			Pin.inUse.push(pin);
 		}
 		this.pin = pin;
+	}
+
+	public release(): void {
+		Pin.inUse.splice(Pin.inUse.indexOf(this.pin), 1);
 	}
 }
 
