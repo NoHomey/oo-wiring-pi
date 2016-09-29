8c124c3f012b46723177361ac6a6c2fbc8af3d91
NoHomey <ivo.stratev.tues@gmail.com>
2016-09-29 12:54:00 +0300

    passing all tests

--- src/InputPin.ts
+++ src/InputPin.ts
@@ -1,6 +1,6 @@
 import Pin from './Pin';
 import ReadablePin from './ReadablePin';
-import { pinMode, INPUT, HIGH, PUD_OFF, digitalRead, pullUpDnControl } from 'wiring-pi';
+import { pinMode, INPUT, HIGH, PUD_OFF, PUD_UP, digitalRead, pullUpDnControl } from 'wiring-pi';
 
 export enum Pull {
     Off,
@@ -12,7 +12,14 @@ export class InputPin extends Pin implements ReadablePin<boolean> {
     public constructor(pin: number, pull: Pull = Pull.Off) {
         super(pin);
         pinMode(pin, INPUT);
-        pullUpDnControl(pin, PUD_OFF);
+        switch(pull) {
+            case Pull.Off:
+                pullUpDnControl(pin, PUD_OFF);
+                break;
+            case Pull.Up:
+                pullUpDnControl(pin, PUD_UP);
+                break;
+        }
     }
 
     public read(): boolean {
