b86f52cdc8391541448834c1061405d3cb256434
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-04 22:44:39 +0300

    testing dose SoftPwmOutputPin throws RangeError if range is less than 1 and re-aranging tests

--- __tests__/SoftPwmOutputPin.spec.ts
+++ __tests__/SoftPwmOutputPin.spec.ts
@@ -4,13 +4,17 @@ import SoftPwmOutputPin from '../src/SoftPwmOutputPin';
 
 describe('SoftPwmOutputPin', () => {
     describe('constructor', () => {
-        it('sets the given pin as SOFT_PWM_OUTPUT', () => {
-            let pin: SoftPwmOutputPin = new SoftPwmOutputPin(1);
-            expect(pinMode).toBeCalledWith(1, SOFT_PWM_OUTPUT);
+        it('throws RangeError if range is less than 1', () => {
+            expect(() => { let pin: SoftPwmOutputPin = new SoftPwmOutputPin(1, 0, 0) }).toThrowError(RangeError);
         });
 
         it('throws RangeError if value is not in range [0..range]', () => {
             expect(() => { let pin: SoftPwmOutputPin = new SoftPwmOutputPin(2, 120, 110) }).toThrowError(RangeError);
+        });
+
+        it('sets the given pin as SOFT_PWM_OUTPUT', () => {
+            let pin: SoftPwmOutputPin = new SoftPwmOutputPin(3);
+            expect(pinMode).toBeCalledWith(3, SOFT_PWM_OUTPUT);
         });
     });
 });
