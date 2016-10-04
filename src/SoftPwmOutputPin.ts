73a3b36b8e4aa1b73b984594c61d6059a1906bcf
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-04 21:47:58 +0300

    pasing last test

--- src/SoftPwmOutputPin.ts
+++ src/SoftPwmOutputPin.ts
@@ -1,8 +1,10 @@
 import Pin from './Pin';
+import { pinMode, SOFT_PWM_OUTPUT } from 'wiring-pi';
 
 export class SoftPwmOutputPin extends Pin {
     public constructor(pin: number) {
         super(pin);
+        pinMode(pin, SOFT_PWM_OUTPUT);
     }
 }
 
