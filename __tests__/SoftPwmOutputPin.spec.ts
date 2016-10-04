f8cc4b3c9c5de117f7fd3f73de6e2c6f4a9eb7ef
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-04 22:47:28 +0300

    testing dose SoftPwmOutputPin throws descriptive RangeError if range is less than 1 and re-aranging tests

--- __tests__/SoftPwmOutputPin.spec.ts
+++ __tests__/SoftPwmOutputPin.spec.ts
@@ -8,6 +8,10 @@ describe('SoftPwmOutputPin', () => {
             expect(() => { let pin: SoftPwmOutputPin = new SoftPwmOutputPin(1, 0, 0) }).toThrowError(RangeError);
         });
 
+        it('throws descriptive RangeError if range is less than 1', () => {
+            expect(() => { let pin: SoftPwmOutputPin = new SoftPwmOutputPin(1, 0, 0) }).toThrowError('range must be positve integer, 0 is less than 1');
+        });
+
         it('throws RangeError if value is not in range [0..range]', () => {
             expect(() => { let pin: SoftPwmOutputPin = new SoftPwmOutputPin(2, 120, 110) }).toThrowError(RangeError);
         });
