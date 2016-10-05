68dfed2ea5f43d7806667609600162516a42b0ef
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-05 19:23:14 +0300

    passing last test

--- src/SoftToneOutputPin.ts
+++ src/SoftToneOutputPin.ts
@@ -4,7 +4,9 @@ import { softToneCreate } from 'wiring-pi';
 export class SoftToneOutputPin extends Pin {
     public constructor(pin: number) {
         super(pin);
-        softToneCreate(pin);
+        if(softToneCreate(pin) !== 0) {
+            throw new Error();
+        }
     }
 }
 
