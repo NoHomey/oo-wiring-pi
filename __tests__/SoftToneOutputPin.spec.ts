504ba9e5057e9afc45a950d5c399e3dfb0503625
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-05 19:25:03 +0300

    test dose SoftToneOutputPin throws descriptive Error if softToneCreate returns non-zero result

--- __tests__/SoftToneOutputPin.spec.ts
+++ __tests__/SoftToneOutputPin.spec.ts
@@ -13,6 +13,11 @@ describe('SoftToneOutputPin', () => {
             expect(() => { let pin: SoftToneOutputPin = new SoftToneOutputPin(1) }).toThrowError(Error);
         });
 
+        it('throws descriptive Error if softToneCreate returns non-zero result', () => {
+            (softToneCreate as SoftToneCreateMock).mockReturnValueOnce(() => -1);
+            expect(() => { let pin: SoftToneOutputPin = new SoftToneOutputPin(2) }).toThrowError('Could not set pin: 2 as SoftwareToneOutputPin');
+        });
+
         it('creates soft tone on the given pin', () => {
             let pin: SoftToneOutputPin = new SoftToneOutputPin(4);
             expect(softToneCreate).toBeCalledWith(4);
