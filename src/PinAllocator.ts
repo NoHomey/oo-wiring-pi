4183b9b43ae7b25c69a65aa17e95f90ff7c2fcfd
NoHomey <ivo.stratev.tues@gmail.com>
2016-10-11 17:13:03 +0300

    passing last test

--- src/PinAllocator.ts
+++ src/PinAllocator.ts
@@ -1,5 +1,6 @@
 import SpecialPins from './SpecialPins/SpecialPins';
 import wpiSpecialPins from './SpecialPins/wpiSpecialPins';
+import bcmSpecialPins from './SpecialPins/bcmSpecialPins';
 import { wiringPiSetup, wiringPiSetupGpio, wiringPiSetupPhys } from 'wiring-pi';
 
 export enum PinEnumerator {
@@ -13,7 +14,10 @@ export class PinAllocator {
 
     public static setup(pinEnumerator: PinEnumerator = PinEnumerator.wpi): void {
         switch(pinEnumerator) {
-            case PinEnumerator.bcm: wiringPiSetupGpio();
+            case PinEnumerator.bcm: {
+                wiringPiSetupGpio();
+                PinAllocator.allSpecialPins = bcmSpecialPins;
+            }
             case PinEnumerator.phys: wiringPiSetupPhys();
             default: {
                 wiringPiSetup();
